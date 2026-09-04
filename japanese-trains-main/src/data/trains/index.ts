d279c3596814ba8823585b36de4a74870c20d249
YellowAquarium <154323272+YellowAquarium@users.noreply.github.com>
2026-09-05 01:41:24 +0800

    Delete japanese-trains-main directory

--- japanese-trains-main/src/data/trains/index.ts
+++
@@ -1,8 +0,0 @@
-import type { TrainTypeConfig } from '../../types/trains';
-
-const cityTrainModules = import.meta.glob('./cities/*.json', {
-	eager: true,
-	import: 'default',
-}) as Record<string, TrainTypeConfig[]>;
-
-export const trains = Object.values(cityTrainModules).flat();
