821e7f776b227b102df7824e51f40a25cb75ad39
YellowAquarium <154323272+YellowAquarium@users.noreply.github.com>
2026-09-05 15:18:24 +0800

    v3.0.1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
 import { trains } from './data/trains';
 
 const MOD_ID = 'japanese-trains';
-const MOD_VERSION = 'v3.0.0';
+const MOD_VERSION = 'v3.0.1';
 const TAG = '[Japanese Trains]';
 
 const api = window.SubwayBuilderAPI;
